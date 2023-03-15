33fe0bc8ebbca8e9abd0e5242b4a5b314bbb001e
bl4ckD0g0 <alexheavy1@gmail.com>
2023-03-15 04:12:05 +0100

    Transfers works fine

--- src/main/java/com/alexdev/bankapp/configuration/BeanConfiguration.java
+++ src/main/java/com/alexdev/bankapp/configuration/BeanConfiguration.java
@@ -3,6 +3,7 @@ package com.alexdev.bankapp.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.alexdev.bankapp.services.TransferServiceImpl;
 import com.alexdev.bankapp.services.UserServiceImpl;
 import com.alexdev.bankapp.services.WalletServiceImpl;
 
@@ -17,6 +18,11 @@ public class BeanConfiguration {
     @Bean
     public WalletServiceImpl getWalletServiceImpl(){
         return new WalletServiceImpl();
+    }
+
+    @Bean
+    public TransferServiceImpl getTransferServiceImpl(){
+        return new TransferServiceImpl();
     }
 
 }
