faee7bb96efabef1c6d5919b201869278d1b1cce
bl4ckD0g0 <alexheavy1@gmail.com>
2023-03-21 01:39:25 +0100

    Needed inheritance

--- src/main/java/com/alexdev/bankapp/entities/Deposit.java
+++ src/main/java/com/alexdev/bankapp/entities/Deposit.java
@@ -1,5 +1,9 @@
 package com.alexdev.bankapp.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "DEPOSIT")
 public class Deposit extends OperationWallet {
-    
 }
