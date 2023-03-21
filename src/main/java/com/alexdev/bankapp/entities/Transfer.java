fff19de368c899398f79100d65c42263c258cac4
bl4ckD0g0 <alexheavy1@gmail.com>
2023-03-21 01:39:44 +0100

    Missing classes

--- src/main/java/com/alexdev/bankapp/entities/Transfer.java
+++ src/main/java/com/alexdev/bankapp/entities/Transfer.java
@@ -1,38 +1,19 @@
 package com.alexdev.bankapp.entities;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+
 import lombok.NoArgsConstructor;
 
-@Data
+
 @Entity
 @Table(name = "TRANSFER")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Transfer {
+public class Transfer extends OperationWallet {
     
-    @Id
-    @Column(name ="ID")
-    @GeneratedValue
-    private long id;
-
     @Column(name ="ID_ORIGIN_ACCOUNT")
     private long originAccount;
-
-    @Column(name ="ID_DESTINY_ACCOUNT")
-    private long destinyAccount;
-
-    @Column(name ="AMOUNT")
-    private BigDecimal amount;
-
-    @Column(name ="TRANSFERDATE")
-    private LocalDateTime transferDate;
 }
