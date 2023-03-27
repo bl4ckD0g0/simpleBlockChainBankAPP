616b2ef3c923bc511d4d4c5f6054c7e34121af76
bl4ckD0g0 <alexheavy1@gmail.com>
2023-03-27 06:26:38 +0200

    UserController it's fine

---
+++ src/test/java/com/alexdev/bankapp/user/UserControllerTest.java
@@ -0,0 +1,68 @@
+package com.alexdev.bankapp.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alexdev.bankapp.user.application.UserController;
+import com.alexdev.bankapp.user.domain.User;
+import com.alexdev.bankapp.user.domain.UserService;
+
+@SpringBootTest
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Test
+    public void testGetAllUsers() {
+        User user1 = new User();
+        user1.setFullName("John Doe");
+        User user2 = new User();
+        user2.setFullName("Jane Doe");
+        List<User> userList = Arrays.asList(user1, user2);
+
+        when(userService.getUsers()).thenReturn(userList);
+
+        List<User> result = userController.getAllUsers();
+
+        verify(userService, times(1)).getUsers();
+        assertThat(result).isEqualTo(userList);
+    }
+
+    @Test
+    public void testGetUser() {
+        User user = new User();
+        user.setId(1L);
+        user.setFullName("John Doe");
+
+        when(userService.getUser(1L)).thenReturn(user);
+        
+        User result = userController.getUser(1L);
+
+        verify(userService, times(1)).getUser(1L);
+        assertThat(result).isEqualTo(user);
+    }
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setFullName("John Doe");
+
+        userController.createUser(user);
+
+        verify(userService, times(1)).createUser(user);
+    }
+}
